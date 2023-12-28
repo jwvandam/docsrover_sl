f60a022e72a7906f67bc8d5dd9053bd3833231eb
jwvandam <154997003+jwvandam@users.noreply.github.com>
2023-12-28 20:53:33 +0100

    Update prompts.py
    language

--- knowledge_gpt/core/prompts.py
+++ knowledge_gpt/core/prompts.py
@@ -2,7 +2,7 @@
 from langchain.prompts import PromptTemplate
 
 ## Use a shorter template to reduce the number of tokens in the prompt
-template = """Create a final answer to the given questions using the provided document excerpts (given in no particular order) as sources. ALWAYS include a "SOURCES" section in your answer citing only the minimal set of sources needed to answer the question. If you are unable to answer the question, simply state that you do not have enough information to answer the question and leave the SOURCES section empty. Use only the provided documents and do not attempt to fabricate an answer.
+template = """Create a final answer to the given questions using the provided document excerpts (given in no particular order) as sources. ALWAYS include a "SOURCES" section in your answer citing only the minimal set of sources needed to answer the question. If you are unable to answer the question, simply state that you do not have enough information to answer the question and leave the SOURCES section empty. Use only the provided documents and do not attempt to fabricate an answer. Answer in the same language as the question is asked.
 
 ---------
 
